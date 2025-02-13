1846959ca6b104cc380f6e885c059a40d70de4dd
sagarhv001 <119564762+sagarhv001@users.noreply.github.com>
2025-02-13 17:33:53 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,11 @@
 uvicorn main:app --host 0.0.0.0 --port 8000 --timeout-keep-alive 300 &
 
 # Wait for FastAPI to be available
-echo "Waiting for FastAPI to start..."
-while ! nc -z 0.0.0.0 8000; do   
-  sleep 1
+until curl --output /dev/null --silent --head --fail http://$BACKEND_HOST:8000/; do
+    echo "Waiting for FastAPI to start..."
+    sleep 2
 done
-echo "FastAPI is running!"
+
 
 # Start Streamlit frontend
 streamlit run app.py --server.port $PORT --server.address 0.0.0.0
