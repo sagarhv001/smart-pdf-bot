80d645d51adf9a5eedb36a9dec38aee22678514c
sagarhv001 <119564762+sagarhv001@users.noreply.github.com>
2025-02-13 19:27:57 +0530

    updated start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
 # Start FastAPI on port 8000
-uvicorn main:app --host 0.0.0.0 --port 8000 &
-
-
-# Wait for FastAPI to start
-sleep 5
-
-
-# Start Streamlit on port 8501
-streamlit run app.py --server.port 8501 --server.address 0.0.0.0
+uvicorn main:app --host 0.0.0.0 --port 8000 
